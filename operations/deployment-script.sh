034a6d925168c86ad907ce7be8157e683079d758
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-06-02 23:49:50 +0000

    updated

--- operations/deployment-script.sh
+++ operations/deployment-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ## Deploy the aplication services using the following commands:
-kubectl apply -k ./mysql
-kubectl apply -k ./wordpress
-kubectl apply -k ./nginx
+kubectl apply -f ./mysql/*.yml
+kubectl apply -f ./wordpress/*.yml
+kubectl apply -f ./nginx/*.yml
