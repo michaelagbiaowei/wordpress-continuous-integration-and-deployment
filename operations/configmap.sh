381a0b23f528cfa43baf3d393ec8ea5d8d4003e2
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-06-02 23:44:49 +0000

    updated

--- operations/configmap.sh
+++ operations/configmap.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+## Apply the configuration services using the following commands:
 kubectl create configmap mysql-configmap --from-file=./mysql/mysql-config.yml
 kubectl create configmap wordpress-configmap --from-file=./wordpress/wordpress-config.yml
 kubectl create configmap nginx-configmap --from-file=./nginx/nginx-config.yml
--- operations/deployment-script.sh
+++ operations/deployment-script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## Deploy the aplication services using the following commands:
 kubectl apply -k ./mysql
 kubectl apply -k ./wordpress
 kubectl apply -k ./nginx
