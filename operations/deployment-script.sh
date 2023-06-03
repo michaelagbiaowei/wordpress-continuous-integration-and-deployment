772e945908f9bc3ea36522ef26bd001010e0ef66
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-06-03 18:59:26 +0000

    updated

--- operations/deployment-script.sh
+++ operations/deployment-script.sh
@@ -2,7 +2,7 @@
 
 ## Deploy the aplication services using the following commands:
 kubectl apply -f ./mysql --prune-allowlist=mysql-configmap.yml
-# kubectl apply -f ./wordpress --prune-allowlist=wordpress-configmap.yml
+kubectl apply -f ./wordpress --prune-allowlist=wordpress-configmap.yml
 # kubectl apply -f ./nginx --prune-allowlist=nginx-configmap.yml --validate=false
 
 
