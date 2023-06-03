331a1a3e38aa1e0fc629d61b276c803772868411
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-06-03 03:51:33 +0000

    updated

--- operations/configmap.sh
+++ operations/configmap.sh
@@ -2,5 +2,5 @@
 
 ## Apply the configuration services using the following commands:
 kubectl create configmap mysql-configmap --from-file=./mysql/mysql-configmap.yml
-kubectl create configmap wordpress-configmap --from-file=./wordpress/wordpress-configmap.yml
-kubectl create configmap nginx-configmap --from-file=./nginx/nginx-configmap.yml
+# kubectl create configmap wordpress-configmap --from-file=./wordpress/wordpress-configmap.yml
+# kubectl create configmap nginx-configmap --from-file=./nginx/nginx-configmap.yml
