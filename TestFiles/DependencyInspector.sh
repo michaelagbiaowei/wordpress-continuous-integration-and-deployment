6798fdb8740dee1ef2b08ff375c6116bfccc9aee
Michael Agbiaowei <bryanwalton1997@gmail.com>
2023-06-19 11:29:49 +0000

    updated

--- TestFiles/DependencyInspector.sh
+++ TestFiles/DependencyInspector.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+url="http://example.com"
+
 echo "Enter the URL of the WordPress site you want to scan:"
 # read url
 
-url="http://example.com"
-
 echo "Scanning $url for vulnerabilities..."
-wpscan --url $url --enumerate vp --plugins-detection mixed --no-color
+
+if ! wpscan --url $url --enumerate vp --plugins-detection mixed --no-color; then
+  echo "Scan failed!"
+  exit 1
+fi
+
+echo "Scan completed successfully!"
