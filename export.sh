9dec0435e4da3b9c58e0dabfa16702bceccb8af7
michaelagbiaowei <bryanwalton1997@gmail.com>
2023-07-07 12:17:26 +0100

    updated

--- export.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export DB_HOST=database-1.c4v46hctkald.us-east-1.rds.amazonaws.com
-export DB_PASSWORD=JeA8BSUivLRGjF4GwVUe
-export DB_NAME=wordpressdb
-export DB_USERNAME=admin
